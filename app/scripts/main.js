618b7edde10096eb7f0ff958c95449ce5b372dcd
Javier Enrique <s0wkete@gmail.com>
2015-08-10 10:31:53 -0300

    testing iterm 2 github setup

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,45 +9,39 @@ var dataset= {
 		medium: [2, 10, 15, 20, 5, 5, 35, 8],
 		high: [2, 10, 10, 10, 5, 5, 48, 10]
 	},
-	// Table with the max value in base of three thirds of 83gb each
-	first: {
-		basic: [4352, 2550, 43516, 283, 850, 850, 0, 0],
-		medium: [8703, 567, 26110, 283, 850, 1062, 33, 340],
-		high: [8703, 567, 17406, 142, 850, 1062, 47, 340]
+	// Table Q of user and his % for profile
+	qOfUsers: [
+		{basic: 2, medium: 6, high: 12},
+		{basic: 2, medium: 7, high: 14},
+		{basic: 3, medium: 8, high: 16},
+		{basic: 3, medium: 8, high: 18},
+		{basic: 4, medium: 9, high: 20},
+		{basic: 4, medium: 10, high: 22},
+		{basic: 4, medium: 11, high: 24},
+		{basic: 5, medium: 12, high: 26},
+		{basic: 5, medium: 12, high: 28},
+		{basic: 6, medium: 13, high: 30},
+	],
+	// Table with the max value (250gb) in GB and Q
+	maxQ: {
+		basic: [13107, 5120, 4864, 1280, 640, 0, 0],
+		medium: [26214, 2389, 2176, 853, 1280, 114, 640],
+		high: [26214, 1365, 1024, 469, 1920, 171, 1024]
 	},
-	second: {
-		basic: [8703, 5100, 87032, 567, 1700, 1700, 0, 0],
-		medium: [17406, 1133, 52219, 567, 1700, 2125, 66, 680],
-		high: [17406, 1133, 34813, 283, 1700, 2125, 94, 680]
-	},
-	third: {
-		basic: [13107, 7680 ,131072, 853, 2560, 2560, 0,0],
-		medium: [26214, 1707, 78643, 853, 2560, 3200, 100, 1024],
-		high: [26214, 1707, 52429, 427, 2560, 3200, 142, 1024]
+	maxGB: {
+		basic: [3, 75, 95, 75, 3, 0, 0],
+		medium: [5, 35, 43, 50, 5, 100, 13],
+		high: [5, 20, 20, 28, 8, 150, 20]
 	},
 	// Conversion table in GB
 	conversion: [0.000190735, 0.01464844, 0.00047684, 0.0585937, 0.00488281, 0.00390625 , 0.87890625, 0.01953125]
 };
 
-// Table Q of user and his % for profile
-var qOfUsers = [
-	{basic: 2, medium: 6, high: 12},
-	{basic: 2, medium: 7, high: 14},
-	{basic: 3, medium: 8, high: 16},
-	{basic: 3, medium: 8, high: 18},
-	{basic: 4, medium: 9, high: 20},
-	{basic: 4, medium: 10, high: 22},
-	{basic: 4, medium: 11, high: 24},
-	{basic: 5, medium: 12, high: 26},
-	{basic: 5, medium: 12, high: 28},
-	{basic: 6, medium: 13, high: 30},
-];
-
 // Obj with the variables selected for the user and the total of the family
 var userSetting = {
-	numOfDisp: 0,
-	basicUser: 0,
-	mediumUser: 0,
+	numOfDisp: 4,
+	basicUser: 1,
+	mediumUser: 1,
 	highUser: 0,
 	total: 0,
 	resultList: []
@@ -55,11 +49,20 @@ var userSetting = {
 
 // Calc the total usage of a family in GB
 var totalUsage = function(obj, data) {
-	var total = (data[obj.numOfDisp -1].basic/100 * obj.basicUser +
-				data[obj.numOfDisp -1].medium/100 * obj.mediumUser +
-				data[obj.numOfDisp -1].high/100 * obj.highUser)*250;
+	var disp = data[obj.numOfDisp -1];
+	var total = (disp.basic/100 * obj.basicUser + disp.medium/100 * obj.mediumUser + disp.high/100 * obj.highUser)*250;
 	// I validate if the total is less than 250gb
 	return total > 250 ? 250 : Math.floor(total);
+};
+
+// Return a list of activies in base Q
+var totalOfQ = function(total, data, type) {
+	// var list = [];
+	return console.log(total, data, type);
+	// for(var i = 0; i < data.type.length; i++) {
+	// 	list.push(Math.ceil(data.type[i] * (total/250)));
+	// }
+	// return list;
 };
 
 // Return a list of activies in base of the total of a family and the kind
@@ -71,8 +74,16 @@ var resultOfactivities = function(total, chart, conversion) {
 	return list;
 };
 
-// userSetting.total = totalUsage(userSetting, qOfUsers);
-// userSetting.resultList = resultOfactivities(166, dataset.charts.basic, dataset.conversion)
+userSetting.total = totalUsage(userSetting, dataset.qOfUsers);
+userSetting.resultList = resultOfactivities(40, dataset.charts.basic, dataset.conversion);
+
+$('.box-bar').each(function(i){
+	//(2098 / 4352) * 100
+	// var progress = Math.floor((userSetting.resultList[i] / dataset.third.medium[i]) * 100);
+	// $(this).find('.progress-bar').css('width', progress + '%');
+	$(this).find('.progress-bar').css('width', dataset.charts.basic[i] + '%');
+ 	$(this).find('.result-list').text(userSetting.resultList[i]);
+ });
 
 // ---------- Devices and user profiles selector ---------- //
 // Number of devices by kind & Number of Users by profile
